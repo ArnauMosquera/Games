d7b6aa3eea667fb2803068b4a2b4e28d9b4cf71e
Arnau <83228781+ArnauMosquera@users.noreply.github.com>
2025-04-28 17:45:38 +0200

    a

--- 3_en_linea/main.c
+++ 3_en_linea/main.c
@@ -1,8 +1,26 @@
-//
-//  main.c
-//  
-//
-//  Created by Arnau Mosquera Alejos on 28/4/25.
-//
-
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 50
+void crear_tauler(int n, int *taula[n][n]){
+    if(n>MAX){
+        printf("La mida del tauler ha de ser igual o inferior al maxim %d",MAX);
+        exit(1);
+    }
+    int tauler[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            tauler[i][j]=rand()%2;
+        }
+    }
+}
+
+
+
+
+int main(void){
+    srand(time(NULL));
+    
+    
+    return 0;
+}
